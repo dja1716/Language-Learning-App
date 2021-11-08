31caf71857b16baae420315da57be024267fa7b9
dja1716 <hdum@uwaterloo.ca>
2021-11-08 20:33:59 +0900

    animated: loop + sequence

--- App.js
+++ App.js
@@ -1,36 +1,60 @@
 import React, { useState, useEffect, useRef } from "react";
 import styled from "styled-components/native";
-import { Animated, TouchableOpacity, Pressable } from "react-native";
+import {
+  Animated,
+  TouchableOpacity,
+  Pressable,
+  Dimensions,
+} from "react-native";
+
+const { width: SCREEN_WIDTH, height: SCREEN_HEIGHT } = Dimensions.get("window");
 
 export default function App() {
-  const [up, setUp] = useState(false);
-  const Y_POSITION = useRef(new Animated.Value(300)).current;
-  const toggleUp = () => setUp((prev) => !prev);
+  const POSITION = useRef(
+    new Animated.ValueXY({
+      x: -SCREEN_WIDTH / 2 + 100,
+      y: -SCREEN_HEIGHT / 2 + 100,
+    })
+  ).current;
+  const topLeft = Animated.timing(POSITION, {
+    toValue: { x: -SCREEN_WIDTH / 2 + 100, y: -SCREEN_HEIGHT / 2 + 100 },
+    useNativeDriver: false,
+  });
+  const bottomLeft = Animated.timing(POSITION, {
+    toValue: { x: -SCREEN_WIDTH / 2 + 100, y: SCREEN_HEIGHT / 2 - 100 },
+    useNativeDriver: false,
+  });
+  const bottomRight = Animated.timing(POSITION, {
+    toValue: { x: SCREEN_WIDTH / 2 - 100, y: SCREEN_HEIGHT / 2 - 100 },
+    useNativeDriver: false,
+  });
+  const topRight = Animated.timing(POSITION, {
+    toValue: { x: SCREEN_WIDTH / 2 - 100, y: -SCREEN_HEIGHT / 2 + 100 },
+    useNativeDriver: false,
+  });
   const moveUp = () => {
-    Animated.timing(Y_POSITION, {
-      toValue: up ? 300 : -300,
-      useNativeDriver: true,
-      duration: 3000,
-    }).start(toggleUp);
+    Animated.loop(
+      Animated.sequence([bottomLeft, bottomRight, topRight, topLeft])
+    ).start();
   };
 
-  const opacity = Y_POSITION.interpolate({
-    inputRange: [-300, 0, 300],
-    outputRange: [1, 0.5, 1],
-  });
-
-  const borderRadius = Y_POSITION.interpolate({
+  const borderRadius = POSITION.y.interpolate({
     inputRange: [-300, 300],
     outputRange: [100, 0],
   });
+  const bgColor = POSITION.y.interpolate({
+    inputRange: [-300, 300],
+    outputRange: ["rgb(255,99,71)", "rgb(71,166,255)"],
+  });
+
   return (
     <Container>
       <Pressable onPress={moveUp}>
         <AnimatedBox
           style={{
-            transform: [{ translateY: Y_POSITION }],
-            opacity,
+            transform: [...POSITION.getTranslateTransform()],
             borderRadius,
+            backgroundColor: bgColor,
           }}
         />
       </Pressable>
