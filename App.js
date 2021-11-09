eda83ee0b6f68d5dc25d48e2b9d2973d015b2615
dja1716 <hdum@uwaterloo.ca>
2021-11-09 12:07:16 +0900

    langApp begin here

--- App.js
+++ App.js
@@ -1,60 +1,15 @@
 import React, { useRef } from "react";
 import styled from "styled-components/native";
 import { Animated, PanResponder } from "react-native";
+import { Ionicons } from "@expo/vector-icons";
 
 export default function App() {
-  const POSITION = useRef(
-    new Animated.ValueXY({
-      x: 0,
-      y: 0,
-    })
-  ).current;
-  const panResponder = useRef(
-    PanResponder.create({
-      onStartShouldSetPanResponder: () => true,
-      onPanResponderMove: (_, { dx, dy }) =>
-        POSITION.setValue({ x: dx, y: dy }),
-      onPanResponderGrant: () => {
-        POSITION.setOffset({ x: POSITION.x._value, y: POSITION.y._value });
-      },
-      onPanResponderRelease: () => {
-        POSITION.flattenOffset();
-      },
-    })
-  ).current;
-
-  const borderRadius = POSITION.y.interpolate({
-    inputRange: [-300, 300],
-    outputRange: [100, 0],
-  });
-  const bgColor = POSITION.y.interpolate({
-    inputRange: [-300, 300],
-    outputRange: ["rgb(255,99,71)", "rgb(71,166,255)"],
-  });
-
-  return (
-    <Container>
-      <AnimatedBox
-        {...panResponder.panHandlers}
-        style={{
-          transform: POSITION.getTranslateTransform(),
-          borderRadius,
-          backgroundColor: bgColor,
-        }}
-      />
-    </Container>
-  );
+  return <Container></Container>;
 }
 
 const Container = styled.View`
   flex: 1;
   justify-content: center;
   align-items: center;
+  background-color: #00a8ff;
 `;
-const Box = styled.View`
-  background-color: tomato;
-  width: 200px;
-  height: 200px;
-`;
-
-const AnimatedBox = Animated.createAnimatedComponent(Box);
